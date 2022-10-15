2a103989787bfb74a6d826ecdc29694f3677be88
Lot Kaduma <42066033+LKadman@users.noreply.github.com>
2022-10-15 05:35:39 +0000

    I added step-by-step descriptions

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Write your code below this line 👇
+
+# Use the print () function to input every line. Press 'Run' to print the output in the console
+print("Hello World!")
+# Quotation marks tells the computer the items to print will not be executed as actual code but just pieces of texts i.e it prints exactly what was written in the brackets. These are known as 'Strings', because the texts are 'strung up' together
+# Take note of subtle hints in the code, if anything is missing or not properly placed eg not adding the quotation marks or closing brackets (parenthesis), the computer alerts you with colour changes.
+
+#print("Hello World!) 
+#running this code will give a Syntax Error message in red color in the console
+#  File "main.py", line 8
+#    print("Hello World!) 
+#                       ^
+#SyntaxError: EOL while scanning string literal
+#Take note of the position of the caret symbol ( ^ ) below the code in the error message
+      
+#To fix an error, copy the error message drop in google and open the result in Stack Overflow to see a list of solutions eg 'You are not putting a " before the end of the line.'
+
+#Syntax Higlighting gives hint of the code structure and prompts on possible error. Always take note of the color of the code. There must be a color match, eg 
